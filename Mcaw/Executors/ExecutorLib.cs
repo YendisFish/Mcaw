4c86b6f3308c48f70a99a771e38b97559ec009fc
YendisFish <brintonsid@gmail.com>
2022-04-17 13:37:12 -0600

    Updated ExecutorLib.cs to support system update

--- Mcaw/Executors/ExecutorLib.cs
+++ Mcaw/Executors/ExecutorLib.cs
@@ -4,7 +4,7 @@ namespace Mcaw.Main.Executors
 {
     public class ExecutorLib
     {
-        public static async Task<TaskOut> BasicExecutor(string input)
+        public static async Task<TaskOut> BasicExecutor(string input, int token = 10000)
         {
             List<string> inp = await Construct.ConstructProgramArgs(input);
             
@@ -19,9 +19,9 @@ namespace Mcaw.Main.Executors
             inf.RedirectStandardOutput = true;
             
             Process? proc = Process.Start(inf);
-
+            
             CancellationTokenSource tsource = new();
-            tsource.CancelAfter(10000);
+            tsource.CancelAfter(token);
 
             await proc.WaitForExitAsync(tsource.Token);
 
@@ -33,10 +33,25 @@ namespace Mcaw.Main.Executors
             if (input.Contains("cd"))
             {
                 List<string> cmd = await Construct.ConstructProgramArgs(input);
-                
+
+                if (cmd[1] == "~" || cmd[1] == "" || cmd[1] == null)
+                {
+                    cmd[1] = "/home/" + Environment.UserName + "/";
+                }
+
                 Directory.SetCurrentDirectory(cmd[1]);
 
                 return new TaskOut("Changed!", Directory.GetCurrentDirectory());
+            }
+
+            if (input.Contains("pacman"))
+            {
+                return await BasicExecutor(input, 10000000);
+            }
+
+            if (input.Contains("cls"))
+            {
+                return await BasicExecutor("clear");
             }
 
             return await BasicExecutor(input);
